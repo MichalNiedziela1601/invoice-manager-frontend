05361988269eb60493fa0315df06162c884b0f6b
Michal Niedziela <MichalNiedziela1601@labnoratory.com>
2017-03-14 22:49:07 +0100

    ITA-96 Frontend/ optimization datapicker addInvoice.ctrl

--- app/modules/addInvoice/addInvoice.controller.js
+++ app/modules/addInvoice/addInvoice.controller.js
@@ -12,99 +12,65 @@
         ctrl.invoicePerson = {};
         ctrl.companyDetails = {};
 
-        ctrl.create = {
-            opened: false
+        ctrl.createDatePicker = {
+            date: new Date(),
+            opened: false,
+            options: {
+                formatYear: 'yy',
+                maxDate: new Date(),
+                startingDay: 1
+            },
+            open: function ()
+            {
+                this.opened = !this.opened;
+            }
         };
 
-        ctrl.expire = {
-            opened: false
+        ctrl.executionDatePicker = {
+            date: new Date(),
+            opened: false,
+            options: {
+                formatYear: 'yy',
+                startingDay: 1
+            },
+            open: function ()
+            {
+                this.opened = !this.opened;
+            }
         };
-
-        ctrl.format = 'dd-MMMM-yyyy';
-
-        ctrl.dateOptionsCreate = {
-            formatYear: 'yy',
-            maxDate: new Date(),
-            startingDay: 1
-        };
-
-        ctrl.dateOptionsExpire = {
-            formatYear: 'yy',
-            startingDay: 1
-        };
-
 
         ////////////////////////////
-        function todayCreate()
-        {
-            ctrl.createDate = new Date();
-        }
-
-        function todayExpire()
-        {
-            ctrl.expireDate = new Date();
-        }
-
-        function clearCreate()
-        {
-            ctrl.createDate = null;
-        }
-
-        function clearExpire()
-        {
-            ctrl.expireDate = null;
-        }
-
-        function openCreate()
-        {
-            ctrl.create.opened = true;
-        }
-
-        function openExpire()
-        {
-            ctrl.expire.opened = true;
-        }
 
         function addInvoiceCompany()
         {
             ctrl.invoiceCompany.type = ctrl.transationType;
-            ctrl.invoiceCompany.createDate = ctrl.createDate.toISOString().slice(0, 10);
-            ctrl.invoiceCompany.executionEndDate = ctrl.expireDate.toISOString().slice(0, 10);
-            Invoice.add(ctrl.invoiceCompany).then(function(){
+            ctrl.invoiceCompany.createDate = ctrl.createDatePicker.date.toISOString().slice(0, 10);
+            ctrl.invoiceCompany.executionEndDate = ctrl.executionDatePicker.date.toISOString().slice(0, 10);
+            Invoice.add(ctrl.invoiceCompany).then(function ()
+            {
 
             });
         }
 
         function addInvoicePerson()
         {
-            ctrl.invoicePerson.createDate = ctrl.createDate.toISOString().slice(0, 10);
-            ctrl.invoicePerson.executionEndDate = ctrl.expireDate.toISOString().slice(0, 10);
+            ctrl.invoicePerson.type = ctrl.transationType;
+            ctrl.invoicePerson.createDate = ctrl.createDatePicker.date.toISOString().slice(0, 10);
+            ctrl.invoicePerson.executionEndDate = ctrl.executionDatePicker.date.toISOString().slice(0, 10);
         }
 
-        function toggleShowCreateCompany(){
+        function toggleShowCreateCompany()
+        {
             ctrl.showCreateCompany = !ctrl.showCreateCompany;
         }
 
-        function init()
+        function findContractor()
         {
-            todayCreate();
-            todayExpire();
-        }
-
-        function findContractor(){
 
         }
 
         ////////////////////////////////////
 
-        init();
-
-        ctrl.todayCreate = todayCreate;
-        ctrl.todayExpire = todayExpire;
-        ctrl.clearCreate = clearCreate;
-        ctrl.clearExpire = clearExpire;
-        ctrl.openCreate = openCreate;
-        ctrl.openExpire = openExpire;
         ctrl.addInvoiceCompany = addInvoiceCompany;
         ctrl.addInvoicePerson = addInvoicePerson;
         ctrl.findContractor = findContractor;
@@ -112,7 +78,7 @@
     }
 
     angular.module('app')
-            .controller('AddInvoiceController', ['Invoice',AddInvoiceController]);
+            .controller('AddInvoiceController', ['Invoice', AddInvoiceController]);
 
 
 })();
