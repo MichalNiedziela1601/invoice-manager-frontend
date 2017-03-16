f65e9ce9e8fe4841cab3193339f64fcd6eeb21a0
Bielbiel <Bartlomiej.Biel@labnoratory.com>
2017-03-16 14:08:25 +0100

    ITA-115 Frontend / Create start page

--- app/modules/registration/registrationCompany.controller.js
+++ app/modules/registration/registrationCompany.controller.js
@@ -1,14 +1,30 @@
 (function ()
 {
     'use strict';
-    function RegistrationCompanyController()
+    function RegistrationCompanyController(AuthDAO)
     {
         var ctrl = this;
         ctrl.message = 'Registration';
 
+        ctrl.registrationCredential =
+                {
+                    companyName: '',
+                    email: '',
+                    nip: '',
+                    password: '',
+                    repeatPassword: ''
+                };
+
+        ctrl.registration = function ()
+        {
+            AuthDAO.registration(ctrl.registrationCredential).then(function ()
+            {
+
+            });
+        };
 
     }
 
-    angular.module('app').controller('RegistrationCompanyController', [RegistrationCompanyController]);
+    angular.module('app').controller('RegistrationCompanyController', ['AuthDAO', RegistrationCompanyController]);
 
 })();
