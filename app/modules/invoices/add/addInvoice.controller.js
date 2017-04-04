406df8be3ad13325079150e2ad54a22a8c74c0e5
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2017-04-04 11:26:39 +0200

    Fix validation in add invoice

--- app/modules/invoices/add/addInvoice.controller.js
+++ app/modules/invoices/add/addInvoice.controller.js
@@ -14,6 +14,8 @@
         ctrl.url = true;
         ctrl.companyNotChosen = false;
         ctrl.noResults = false;
+        ctrl.formInvalidAlert = false;
+        ctrl.formSubmitted = false;
 
         ctrl.createDatePicker = {
             date: new Date(), opened: false, options: {
@@ -45,7 +47,7 @@
             }
         }
 
-        function addInvoiceCompany()
+        function addInvoiceCompany(form)
         {
             if (ctrl.companyDetails) {
                 ctrl.invoiceCompany.type = ctrl.transationType;
@@ -60,19 +62,28 @@
                         file: ctrl.file
                     }
                 };
-                Upload.upload(ctrl.fileToUpload).then(function ()
-                {
-                    ctrl.companyNotChosen= false;
-                    ctrl.objectURL = URL.createObjectURL(ctrl.file);
-                    ctrl.addInvoice = true;
-                    ctrl.createDatePicker.date = new Date();
-                    ctrl.executionDatePicker.date = new Date();
-                    ctrl.transationType = null;
-                    ctrl.invoiceCompany = {};
-                }).catch(function (error)
-                {
-                    console.error(error);
-                });
+                if(form.$valid) {
+                    if(!ctrl.formSubmitted) {
+                        ctrl.formSubmitted = true;
+                        console.log(ctrl.invoiceCompany);
+                        Upload.upload(ctrl.fileToUpload).then(function ()
+                        {
+                            ctrl.companyNotChosen = false;
+                            ctrl.objectURL = URL.createObjectURL(ctrl.file);
+                            ctrl.addInvoice = true;
+                            ctrl.createDatePicker.date = new Date();
+                            ctrl.executionDatePicker.date = new Date();
+                            ctrl.transationType = null;
+                            ctrl.invoiceCompany = {};
+                            form.$setPristine();
+                            ctrl.formSubmitted = false;
+                        }).catch(function (error)
+                        {
+                            ctrl.errorMessage = error.data;
+                            ctrl.formInvalidAlert = !ctrl.formInvalidAlert;
+                        });
+                    }
+                }
             } else {
                 ctrl.companyNotChosen= true;
             }
@@ -134,6 +145,10 @@
             ctrl.addInvoice = false;
         }
 
+        function closeFormInvalidAlert(){
+            ctrl.formInvalidAlert = !ctrl.formInvalidAlert;
+        }
+
         function findCompaniesByNip(nip)
         {
             return CompanyDAO.getNips(nip).then(function (response)
@@ -166,6 +181,7 @@
         ctrl.findCompaniesByNip = findCompaniesByNip;
         $scope.onSelect = onSelect;
         ctrl.getUserInfo = getUserInfo;
+        ctrl.closeInvalidFormAlert = closeFormInvalidAlert;
 
     }
 
